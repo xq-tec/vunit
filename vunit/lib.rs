6d1745192db5b872c3e0e6f78e999d4f1054c61d
Kai Giebeler <giebeler@xq-tec.com>
2026-06-11 03:16:58 +0200

    init rust project

---
+++ vunit/lib.rs
@@ -0,0 +1,3 @@
+//! This crate is a rust port of the VUnit VHDL/SystemVerilog unit testing framework.
+//!
+//! The purpose of this crate is to make the VUnit functionality available as a library for other projects.
---
+++ vunit/ui/main.rs
@@ -0,0 +1,9 @@
+//! This crate makes VUnit available as a command line tool.
+//!
+
+// TODO at the moment this is at an awkward location in the file system until the translation of the Python code to Rust is complete.
+// The responsible Cargo.toml can be found in the project's root.
+
+fn main() {
+    println!("Hello, VUnit!");
+}
